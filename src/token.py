6eaa8cb9fb6224f01d64dc94441dc1901d971006
Joel Lefkowitz <joellefkowitz@hotmail.com>
2020-12-23 18:19:26 +0000

    Add retry wrappers for client methods

--- src/token.py
+++ src/token.py
@@ -1,8 +1,13 @@
 import json
 import logging
 
+import requests
 from hvac import Client
 from retry import retry
+
+logger = logging.getLogger(__name__)
+exceptions = (FileNotFoundError, requests.exceptions.ConnectionError)
+soft_retry = retry(exceptions, tries=10, delay=1, backoff=2, logger=logger)
 
 
 class TokenClient(Client):
@@ -10,14 +15,16 @@ class TokenClient(Client):
         super().__init__(*args, **kwargs)
         self.token = token
 
+    @soft_retry
+    def list(self, path):
+        return super().__init__(path)
+
+    @soft_retry
+    def write(self, path, wrap_ttl=None, **kwargs):
+        return super().__init__(path, wrap_ttl, **kwargs)
+
     @classmethod
-    @retry(
-        (FileNotFoundError, ConnectionError),
-        tries=10,
-        delay=1,
-        backoff=2,
-        logger=logging.getLogger(__name__),
-    )
+    @soft_retry
     def try_token_path(cls, path):
         token = cls.parse_token(path)
         return cls(token)
