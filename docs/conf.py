024fc49e4b2e441fd8727052d8312f20b78b2384
Joel Lefkowitz <joellefkowitz@hotmail.com>
2020-12-24 00:24:00 +0000

    Bump version: 0.3.4 → 0.3.5

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ _package = "buildbot_abstract"
 
 # Sphinx settings
 project = _title
-version = "0.3.4"
+version = "0.3.5"
 master_doc = "index"
 author = "Joel Lefkowitz"
 copyright = f"{datetime.datetime.now().year}, {author}"
